bdcc60b78375599e110739a47b9d1d746d6d4dd7
Nithin Mahesh <nmahesh@constantcontact.com>
2016-06-03 17:53:05 -0400

    Finished the Nav bar and Header title styling, need to add in links to the nav and all the javascript actions

--- myApp/.meteor/local/build/programs/web.browser/app/app.js
+++ myApp/.meteor/local/build/programs/web.browser/app/app.js
@@ -1,88 +1,64 @@
 var require = meteorInstall({"client":{"main.html":["./template.main.js",function(require,exports,module){
 
-///////////////////////////////////////////////////////////////////////
-//                                                                   //
-// client/main.html                                                  //
-//                                                                   //
-///////////////////////////////////////////////////////////////////////
-                                                                     //
-module.exports = require("./template.main.js");                      // 1
-                                                                     // 2
-///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                       //
+// client/main.html                                                                      //
+//                                                                                       //
+///////////////////////////////////////////////////////////////////////////////////////////
+                                                                                         //
+module.exports = require("./template.main.js");                                          // 1
+                                                                                         // 2
+///////////////////////////////////////////////////////////////////////////////////////////
 
 }],"template.main.js":function(){
 
-///////////////////////////////////////////////////////////////////////
-//                                                                   //
-// client/template.main.js                                           //
-//                                                                   //
-///////////////////////////////////////////////////////////////////////
-                                                                     //
-                                                                     // 1
-Template.body.addContent((function() {                               // 2
-  var view = this;                                                   // 3
-  return [ HTML.Raw("<h1>Welcome to the Work Tracker</h1>\n\n  "), Spacebars.include(view.lookupTemplate("hello")), "\n  ", Spacebars.include(view.lookupTemplate("info")) ];
-}));                                                                 // 5
-Meteor.startup(Template.body.renderToDocument);                      // 6
-                                                                     // 7
-Template.__checkName("hello");                                       // 8
-Template["hello"] = new Template("Template.hello", (function() {     // 9
-  var view = this;                                                   // 10
-  return "";                                                         // 11
-}));                                                                 // 12
-                                                                     // 13
-Template.__checkName("info");                                        // 14
-Template["info"] = new Template("Template.info", (function() {       // 15
-  var view = this;                                                   // 16
-  return "";                                                         // 17
-}));                                                                 // 18
-                                                                     // 19
-///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                       //
+// client/template.main.js                                                               //
+//                                                                                       //
+///////////////////////////////////////////////////////////////////////////////////////////
+                                                                                         //
+                                                                                         // 1
+Template.body.addContent((function() {                                                   // 2
+  var view = this;                                                                       // 3
+  return [ HTML.Raw('<h1>Welcome to the Work Tracker</h1>\n    \n    \n    <ul>\n        <li><a href="">Reminders</a></li>\n        <li><a href="">Views</a></li>\n        <li><a href=""></a></li>\n        <li style="float:right"><a class="active" href="">About Company</a></li>\n    </ul>\n    \n     '), HTML.UL("\n      ", Blaze.Each(function() {
+    return Spacebars.call(view.lookup("tasks"));                                         // 5
+  }, function() {                                                                        // 6
+    return [ "\n        ", Spacebars.include(view.lookupTemplate("task")), "\n      " ];
+  }), "\n    ") ];                                                                       // 8
+}));                                                                                     // 9
+Meteor.startup(Template.body.renderToDocument);                                          // 10
+                                                                                         // 11
+Template.__checkName("hello");                                                           // 12
+Template["hello"] = new Template("Template.hello", (function() {                         // 13
+  var view = this;                                                                       // 14
+  return "";                                                                             // 15
+}));                                                                                     // 16
+                                                                                         // 17
+Template.__checkName("info");                                                            // 18
+Template["info"] = new Template("Template.info", (function() {                           // 19
+  var view = this;                                                                       // 20
+  return "";                                                                             // 21
+}));                                                                                     // 22
+                                                                                         // 23
+///////////////////////////////////////////////////////////////////////////////////////////
 
 },"main.js":["meteor/templating","meteor/reactive-var","./main.html",function(require){
 
-///////////////////////////////////////////////////////////////////////
-//                                                                   //
-// client/main.js                                                    //
-//                                                                   //
-///////////////////////////////////////////////////////////////////////
-                                                                     //
-var _templating = require('meteor/templating');                      // 1
-                                                                     //
-var _reactiveVar = require('meteor/reactive-var');                   // 2
-                                                                     //
-require('./main.html');                                              // 4
-                                                                     //
-_templating.Template.hello.onCreated(function () {                   // 6
-  function helloOnCreated() {                                        // 6
-    // counter starts at 0                                           //
-    this.counter = new _reactiveVar.ReactiveVar(0);                  // 8
-  }                                                                  //
-                                                                     //
-  return helloOnCreated;                                             //
-}());                                                                //
-                                                                     //
-_templating.Template.hello.helpers({                                 // 11
-  counter: function () {                                             // 12
-    function counter() {                                             //
-      return _templating.Template.instance().counter.get();          // 13
-    }                                                                //
-                                                                     //
-    return counter;                                                  //
-  }()                                                                //
-});                                                                  //
-                                                                     //
-_templating.Template.hello.events({                                  // 17
-  'click button': function () {                                      // 18
-    function clickButton(event, instance) {                          //
-      // increment the counter when button is clicked                //
-      instance.counter.set(instance.counter.get() + 1);              // 20
-    }                                                                //
-                                                                     //
-    return clickButton;                                              //
-  }()                                                                //
-});                                                                  //
-///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                       //
+// client/main.js                                                                        //
+//                                                                                       //
+///////////////////////////////////////////////////////////////////////////////////////////
+                                                                                         //
+var _templating = require('meteor/templating');                                          // 1
+                                                                                         //
+var _reactiveVar = require('meteor/reactive-var');                                       // 2
+                                                                                         //
+require('./main.html');                                                                  // 4
+                                                                                         //
+_templating.Template.body.helpers({});                                                   // 6
+///////////////////////////////////////////////////////////////////////////////////////////
 
 }]}},{"extensions":[".js",".json",".html",".css"]});
 require("./client/template.main.js");
