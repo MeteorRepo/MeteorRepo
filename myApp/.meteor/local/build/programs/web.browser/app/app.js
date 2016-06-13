86fde00eb08a590770a85a2a9d7fe5f3c06e2158
Nithin Mahesh <nmahesh@constantcontact.com>
2016-06-13 11:53:20 -0400

    This is a branch just to mess around, adding simple changes to the header

--- myApp/.meteor/local/build/programs/web.browser/app/app.js
+++ myApp/.meteor/local/build/programs/web.browser/app/app.js
@@ -1,64 +1,94 @@
 var require = meteorInstall({"client":{"main.html":["./template.main.js",function(require,exports,module){
 
-///////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                       //
-// client/main.html                                                                      //
-//                                                                                       //
-///////////////////////////////////////////////////////////////////////////////////////////
-                                                                                         //
-module.exports = require("./template.main.js");                                          // 1
-                                                                                         // 2
-///////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                        //
+// client/main.html                                                                                       //
+//                                                                                                        //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                                                                          //
+module.exports = require("./template.main.js");                                                           // 1
+                                                                                                          // 2
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }],"template.main.js":function(){
 
-///////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                       //
-// client/template.main.js                                                               //
-//                                                                                       //
-///////////////////////////////////////////////////////////////////////////////////////////
-                                                                                         //
-                                                                                         // 1
-Template.body.addContent((function() {                                                   // 2
-  var view = this;                                                                       // 3
-  return [ HTML.Raw('<h1>Welcome to the Work Tracker</h1>\n    \n    \n    <ul>\n        <li><a href="">Reminders</a></li>\n        <li><a href="">Views</a></li>\n        <li><a href=""></a></li>\n        <li style="float:right"><a class="active" href="">About Company</a></li>\n    </ul>\n    \n     '), HTML.UL("\n      ", Blaze.Each(function() {
-    return Spacebars.call(view.lookup("tasks"));                                         // 5
-  }, function() {                                                                        // 6
-    return [ "\n        ", Spacebars.include(view.lookupTemplate("task")), "\n      " ];
-  }), "\n    ") ];                                                                       // 8
-}));                                                                                     // 9
-Meteor.startup(Template.body.renderToDocument);                                          // 10
-                                                                                         // 11
-Template.__checkName("hello");                                                           // 12
-Template["hello"] = new Template("Template.hello", (function() {                         // 13
-  var view = this;                                                                       // 14
-  return "";                                                                             // 15
-}));                                                                                     // 16
-                                                                                         // 17
-Template.__checkName("info");                                                            // 18
-Template["info"] = new Template("Template.info", (function() {                           // 19
-  var view = this;                                                                       // 20
-  return "";                                                                             // 21
-}));                                                                                     // 22
-                                                                                         // 23
-///////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                        //
+// client/template.main.js                                                                                //
+//                                                                                                        //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                                                                          //
+                                                                                                          // 1
+Template.body.addContent((function() {                                                                    // 2
+  var view = this;                                                                                        // 3
+  return HTML.Raw('<h1>Welcome to the Work Tracker</h1>\n    \n    \n    <ul>\n        <li><a href="">Reminders</a></li>\n        <li><a href="">Views</a></li>\n        <li><a href=""></a></li>\n        <li style="float:right"><a class="active" href="">About Company</a></li>\n    </ul>');
+}));                                                                                                      // 5
+Meteor.startup(Template.body.renderToDocument);                                                           // 6
+                                                                                                          // 7
+Template.__checkName("hello");                                                                            // 8
+Template["hello"] = new Template("Template.hello", (function() {                                          // 9
+  var view = this;                                                                                        // 10
+  return "";                                                                                              // 11
+}));                                                                                                      // 12
+                                                                                                          // 13
+Template.__checkName("info");                                                                             // 14
+Template["info"] = new Template("Template.info", (function() {                                            // 15
+  var view = this;                                                                                        // 16
+  return "";                                                                                              // 17
+}));                                                                                                      // 18
+                                                                                                          // 19
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 },"main.js":["meteor/templating","meteor/reactive-var","./main.html",function(require){
 
-///////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                       //
-// client/main.js                                                                        //
-//                                                                                       //
-///////////////////////////////////////////////////////////////////////////////////////////
-                                                                                         //
-var _templating = require('meteor/templating');                                          // 1
-                                                                                         //
-var _reactiveVar = require('meteor/reactive-var');                                       // 2
-                                                                                         //
-require('./main.html');                                                                  // 4
-                                                                                         //
-_templating.Template.body.helpers({});                                                   // 6
-///////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                        //
+// client/main.js                                                                                         //
+//                                                                                                        //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                                                                          //
+var _templating = require('meteor/templating');                                                           // 1
+                                                                                                          //
+var _reactiveVar = require('meteor/reactive-var');                                                        // 2
+                                                                                                          //
+require('./main.html');                                                                                   // 4
+                                                                                                          //
+if (Meteor.isClient) {                                                                                    // 6
+    _templating.Template.messages.helpers({                                                               // 7
+        messages: function () {                                                                           // 8
+            function messages() {                                                                         // 8
+                return Messages.find({}, { sort: { time: -1 } });                                         // 9
+            }                                                                                             //
+                                                                                                          //
+            return messages;                                                                              //
+        }()                                                                                               //
+    });                                                                                                   //
+                                                                                                          //
+    _templating.Template.input.events = {                                                                 // 13
+        'keydown input#message': function () {                                                            // 14
+            function keydownInputMessage(event) {                                                         // 14
+                if (event.which == 13) {                                                                  // 15
+                    if (Meteor.user()) var name = Meteor.user().profile.name;else var name = 'Anonymous';
+                    var message = document.getElementById('message');                                     // 20
+                    if (message.value != '') {                                                            // 21
+                        Messages.insert({                                                                 // 22
+                            name: name,                                                                   // 23
+                            message: message.value,                                                       // 24
+                            time: Date.now()                                                              // 25
+                        });                                                                               //
+                                                                                                          //
+                        document.getElementById('message').value = '';                                    // 28
+                        message.value = '';                                                               // 29
+                    }                                                                                     //
+                }                                                                                         //
+            }                                                                                             //
+                                                                                                          //
+            return keydownInputMessage;                                                                   //
+        }()                                                                                               //
+                                                                                                          //
+    };                                                                                                    //
+}                                                                                                         //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }]}},{"extensions":[".js",".json",".html",".css"]});
 require("./client/template.main.js");
--- myApp/client/main.js
+++ myApp/client/main.js
@@ -3,8 +3,34 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.body.helpers({
+if(Meteor.isClient) {
+    Template.messages.helpers({
+        messages: function() {
+            return Messages.find({}, {sort: {time: -1}});
+        }
+    });
     
-    
-})
+    Template.input.events = {
+        'keydown input#message' : function (event) {
+            if(event.which == 13) {
+                if(Meteor.user())
+                    var name = Meteor.user().profile.name;
+                else 
+                    var name = 'Anonymous';
+                var message = document.getElementById('message');
+                if(message.value != '') {
+                    Messages.insert({
+                        name: name,
+                        message: message.value,
+                        time: Date.now(),
+                    })
+                    
+                    document.getElementById('message').value = '';
+                    message.value = '';
+                }
+            }
+        }
+        
+    }
+}
 
