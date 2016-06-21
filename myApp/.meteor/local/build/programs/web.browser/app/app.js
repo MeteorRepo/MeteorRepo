efce5990a7b3189e455277f81fbdf63518088018
Nithin Mahesh <nmahesh@constantcontact.com>
2016-06-21 14:18:23 -0400

    began to add in javascript backend functionality, everything still works

--- myApp/.meteor/local/build/programs/web.browser/app/app.js
+++ myApp/.meteor/local/build/programs/web.browser/app/app.js
@@ -26,10 +26,12 @@ Meteor.startup(Template.body.renderToDocument);                      // 6
                                                                      // 1
 Template.body.addContent((function() {                               // 2
   var view = this;                                                   // 3
-  return HTML.Raw('<h1>Welcome to the Work Tracker</h1>\n    \n    \n    <ul>\n        <li><a href="reminders.html">Reminders</a></li>\n        <li><a href="">Views</a></li>\n       <!-- <li><a href=""></a></li> -->\n        <li style="float:right"><a class="active" href="">About Company</a></li>\n    </ul>\n    \n    <br>\n    <br>\n    \n    <input id="task"><button id="add" class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored"><i class="material-icons">Add</i></button>');
-}));                                                                 // 5
-Meteor.startup(Template.body.renderToDocument);                      // 6
-                                                                     // 7
+  return [ HTML.Raw('<h1>Welcome to the Work Tracker</h1>\n    \n    \n    <ul>\n        <li><a href="reminders.html">Reminders</a></li>\n        <li><a href="">Views</a></li>\n       <!-- <li><a href=""></a></li> -->\n        <li style="float:right"><a class="active" href="">About Company</a></li>\n    </ul>\n    \n    <br>\n    <br>\n    \n    <input id="task"><button id="add" class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored"><i class="material-icons">Add</i></button>\n    \n    <div id="todos"></div>\n    \n    '), HTML.SCRIPT({
+    src: "main.js"                                                   // 5
+  }) ];                                                              // 6
+}));                                                                 // 7
+Meteor.startup(Template.body.renderToDocument);                      // 8
+                                                                     // 9
 ///////////////////////////////////////////////////////////////////////
 
 },"main.js":function(){
@@ -40,7 +42,14 @@ Meteor.startup(Template.body.renderToDocument);                      // 6
 //                                                                   //
 ///////////////////////////////////////////////////////////////////////
                                                                      //
+function saveUserInput() {                                           // 1
+    var UserInput = document.getElementById("task");                 // 2
+    var todos = document.getElementById("todos");                    // 3
+}                                                                    //
                                                                      //
+function print(UserInput) {                                          // 6
+    UserInput.append();                                              // 7
+}                                                                    //
 ///////////////////////////////////////////////////////////////////////
 
 }}},{"extensions":[".js",".json",".html",".css"]});
---
+++ myApp/client/main.js
@@ -0,0 +1,8 @@
+function saveUserInput() {
+    var UserInput = document.getElementById("task");
+    var todos = document.getElementById("todos");
+}
+
+function print(UserInput) {
+    UserInput.append();
+}
